764a802d78ce0e5cd4aadbc0dee9ceeda6ba297f
Spencer Kekauoha <spkekauoha@gmail.com>
2016-09-12 20:11:27 -0600

    landing desktop version done

--- app/components/nav/scrollDir.js
+++ app/components/nav/scrollDir.js
@@ -8,7 +8,10 @@ angular.module('port')
             section3Btn = document.getElementById("section3Btn"),
             section4Btn = document.getElementById("section4Btn"),
             section5Btn = document.getElementById("section5Btn"),
-            portBtn = document.getElementById("portBtn");
+            portBtn = document.getElementById("portBtn"),
+            horz1Btn = document.getElementById("horz1Btn"),
+            horz2Btn = document.getElementById("horz2Btn"),
+            horz3Btn = document.getElementById("horz3Btn");
 
         section1Btn.onclick = function() {
           TweenLite.to(window, 1, {scrollTo:{y:"#section1", offsetY:0}});
@@ -31,7 +34,19 @@ angular.module('port')
         };
 
         portBtn.onclick = function() {
+          TweenLite.to(window, 1, {scrollTo:{y:"#section1", offsetY:0}});
+        };
+
+        horz1Btn.onclick = function() {
           TweenLite.to(window, 1, {scrollTo:{y:"#section2", offsetY:0}});
+        };
+
+        horz2Btn.onclick = function() {
+          TweenLite.to(window, 1, {scrollTo:{y:"#section3", offsetY:0}});
+        };
+
+        horz3Btn.onclick = function() {
+          TweenLite.to(window, 1, {scrollTo:{y:"#section4", offsetY:0}});
         };
       }
     };
